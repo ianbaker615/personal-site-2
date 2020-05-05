6a46ce9076f5e3b4635c6557923a33e094f60c90
Ian Baker <ian.baker@iinteractive.com>
2020-05-05 19:16:45 -0400

    Updates

---
+++ precache-manifest.d8d9189523da5ea7e79d492a79b5eeea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5958bbea90ba592223312a4827e21f02",
+    "url": "/personal-site-2/index.html"
+  },
+  {
+    "revision": "0d31af7739e30edee1db",
+    "url": "/personal-site-2/static/css/main.2f50468d.chunk.css"
+  },
+  {
+    "revision": "0805a7d16fc9f3fb0362",
+    "url": "/personal-site-2/static/js/2.a2df23f9.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/personal-site-2/static/js/2.a2df23f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d31af7739e30edee1db",
+    "url": "/personal-site-2/static/js/main.53496887.chunk.js"
+  },
+  {
+    "revision": "04ec8546bc593c845f3f",
+    "url": "/personal-site-2/static/js/runtime-main.7f78856f.js"
+  }
+]);
