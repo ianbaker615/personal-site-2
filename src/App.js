61608ba3f433a619395ef63895808ff2e90524db
Ian Baker <ian.baker@iinteractive.com>
2020-05-04 21:31:53 -0400

    Add responsive font size to app. Refactor nav bar to resize at properly

--- src/App.js
+++ src/App.js
@@ -6,23 +6,41 @@ import Projects from "./components/Projects/Projects";
 import Resume from "./components/Resume/Resume";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { AppBar, Toolbar, Typography, Tab, Tabs } from "@material-ui/core";
+import { makeStyles, createMuiTheme, responsiveFontSizes, ThemeProvider } from '@material-ui/core/styles';
+
+let theme = createMuiTheme();
+theme = responsiveFontSizes(theme);
+
+const useStyles = makeStyles((theme) => ({
+  toolbar: {
+    justifyContent: 'space-between',
+    overflowX: 'auto',
+  },
+  toolbarLink: {
+    padding: theme.spacing(1),
+    flexShrink: 0,
+  },
+}));
 
 function App() {
+  const classes = useStyles();
+
   return (
     <Router>
       <div className="App">
+        <ThemeProvider theme={theme}>
         <nav>
           <AppBar position="relative">
-            <Toolbar className="toolbar">
+            <Toolbar component="nav" variant="dense" className={classes.toolbar}>
               <Typography variant="h5" style={{ flex: 1 }}>
-                <Link style={{ textDecoration: "none", color: "white" }} to="/">
+                <Link style={{ textDecoration: "none", color: "white" }} to="/" >
                   Ian Baker // Software Engineer
                 </Link>
               </Typography>
-              <Tabs>
-                <Tab label="Resume" to="/resume" component={Link} />
-                <Tab label="About Me" to="/aboutme" component={Link} />
-                <Tab label="Projects" to="/projects" component={Link} />
+              <Tabs className={classes.toolbarLink}>
+                <Tab label="Resume" to="/resume" component={Link} className={classes.toolbarLink} />
+                <Tab label="About Me" to="/aboutme" component={Link} className={classes.toolbarLink} />
+                <Tab label="Projects" to="/projects" component={Link} className={classes.toolbarLink} />
               </Tabs>
             </Toolbar>
           </AppBar>
@@ -33,6 +51,7 @@ function App() {
           <Route path="/aboutme" component={AboutMe} />
           <Route path="/resume" component={Resume} />
         </Switch>
+        </ThemeProvider>
       </div>
     </Router>
   );
